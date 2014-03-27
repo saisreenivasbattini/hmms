879b5c2d4847dcf17d4ba3bc18495a6f518a5fba
sreenivas <srinu0904@gmail.com>
2014-03-27 18:27:45 +0530

    added html

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140320055035) do
+
+  create_table "regions", force: true do |t|
+    t.string   "name"
+    t.string   "region_state_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "states", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
