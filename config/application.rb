1ad7f611b02b50e0975a59bd01b508560b162ed2
System Administrator <root@tulasees-MacBook-Pro.local>
2014-03-29 01:39:16 -0400

    added static assets

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,28 @@ module Health
     # config.i18n.default_locale = :de
     config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
     # config.assets.precompile += ['style.css','style3.css','bootstrap.min.css']
-    config.assets.initialize_on_precompile = true
+   config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif,
+                                  "fontawesome-webfont.ttf",
+                                 "fontawesome-webfont.eot",
+                                 "fontawesome-webfont.svg",
+                                 "fontawesome-webfont.woff")
+
+
+
+config.assets.precompile << Proc.new do |path|
+      if path =~ /\.(css|js)\z/
+        full_path = Rails.application.assets.resolve(path).to_path
+        app_assets_path = Rails.root.join('app', 'assets').to_path
+        if full_path.starts_with? app_assets_path
+          puts "including asset: " + full_path
+          true
+        else
+          puts "excluding asset: " + full_path
+          false
+        end
+      else
+        false
+      end
+    end
   end
 end
