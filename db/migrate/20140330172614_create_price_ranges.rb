af16260a7dcfbdf0453fbf8e257e5c8f2c4a78f3
System Administrator <root@tulasees-MacBook-Pro.local>
2014-03-30 22:07:13 -0400

    Added provider search

--- db/migrate/20140330172614_create_price_ranges.rb
+++ db/migrate/20140330172614_create_price_ranges.rb
@@ -1,6 +1,6 @@
 class CreatePriceRanges < ActiveRecord::Migration
   def change
-    drop_table :price_ranges
+    # drop_table :price_ranges
     create_table :price_ranges do |t|
       t.integer :low
       t.integer :high
